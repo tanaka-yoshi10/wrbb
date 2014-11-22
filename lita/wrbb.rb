59a497a0056f8b337436567cbd760b86402c2799
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-11-22 16:46:14 +0900

    led制御可能に

--- lita/wrbb.rb
+++ lita/wrbb.rb
@@ -9,26 +9,32 @@ delay(300)
 loop do
   while(Serial.available(0) > 0) do
     c = Serial.read(0).chr
-    if c == "q"
+    case c
+    when "q"
       Sys.fileload()
+    when "t"
+      l = I2c.request(@APTemp, 2)
+      #Serial.println(0, l.to_s)
+
+      s1 = I2c.lread()
+      #Serial.println(0, s1.inspect)
+      s2 = I2c.lread()
+      #Serial.println(0, s2.inspect)
+      temp_h, temp_l = s1, s2
+      temp = ((temp_h << 4) + (temp_l >> 4)) * 0.0625
+
+      t = temp.round(1).to_s
+      Serial.println(0, t)
+    when "l"
+      Serial.println(0, sw.to_s)
+    when "1"
+      sw = 1
+    when "0"
+      sw = 0
     end
-
-    l = I2c.request(@APTemp, 2)
-    #Serial.println(0, l.to_s)
-
-    s1 = I2c.lread()
-    #Serial.println(0, s1.inspect)
-    s2 = I2c.lread()
-    #Serial.println(0, s2.inspect)
-    temp_h, temp_l = s1, s2
-    temp = ((temp_h << 4) + (temp_l >> 4)) * 0.0625
-
-    t = temp.round(1).to_s
-    Serial.println(0, t)
   end
 
   led(sw)
-  sw = 1 - sw
 
   delay(500)
 end
